609ce9ebd8ed13a8629b4492232481acd4d3db14
nana safo <brudexgh@gmail.com>
2020-09-30 08:45:54 +0000

    added lost packages

--- NetCore/ZenExpresso/ZenExpresso/Extensions/DedicatedAdminsAllowedAttribute.cs
+++ NetCore/ZenExpresso/ZenExpresso/Extensions/DedicatedAdminsAllowedAttribute.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
--- NetCore/ZenExpresso/ZenExpressoCore/TaskFlows/RestApiTaskFlowItem.cs
+++ NetCore/ZenExpresso/ZenExpressoCore/TaskFlows/RestApiTaskFlowItem.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BrudexFoundations;
-using Jint;
+ 
 using Newtonsoft.Json.Linq;
 using ZenExpressoCore.Models;
 
