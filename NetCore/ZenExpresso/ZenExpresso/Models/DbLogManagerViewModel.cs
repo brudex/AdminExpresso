40fc71f5c709a31f97d4c866515ceb692ed5fb4a
NanaSafo <brudexgh@gmail.com>
2020-10-11 18:08:14 +0000

    fixed razor issues

---
+++ NetCore/ZenExpresso/ZenExpresso/Models/DbLogManagerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ZenExpressoCore.Models;
+
+namespace ZenExpresso.Models
+{
+    public class DbLogManagerViewModel
+    {
+        public DbLogManagerViewModel()
+        {
+            appLogs = new List<LogManagement>();
+        }
+
+        public List<LogManagement> appLogs { get; set; }
+    }
+}
