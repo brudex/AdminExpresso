008be156ad16dbe5aa96c24db70ad7c8298863ef
nana safo <brudexgh@gmail.com>
2022-01-14 01:45:42 +0000

    install completed viw

---
+++ ZenExpressoCore/MongoDbhandler.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using Newtonsoft.Json.Linq;
+using ZenExpressoCore.Models;
+
+
+namespace ZenExpressoCore
+{
+    public class MongoDbhandler
+    {
+        static MongoDbhandler instance = null;
+        static readonly object padlock = new object();
+        private IMongoDatabase _db;
+
+        MongoDbhandler()
+        {
+        }
+
+        public static MongoDbhandler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new MongoDbhandler();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public MongoDbhandler GetDbConnection(string userId, string pass, string ip, string dbName = "admin")
+        {
+            string conString = "";
+            string port = "27017";
+            if (string.IsNullOrEmpty(userId))
+            {
+                conString = $"mongodb://{ip}:{port}/{dbName}";
+            }
+            else
+            {
+                conString = $"mongodb://{userId}:{pass}@{ip}{port}/{dbName}";
+            }
+
+            MongoClient client = new MongoClient(conString);
+            _db = client.GetDatabase(dbName);
+            return this;
+        }
+
+        public MongoDbhandler GetDbConnection(string conString, string dbName)
+        {
+            MongoClient client = new MongoClient(conString);
+            if (string.IsNullOrEmpty(dbName))
+            {
+                dbName = "admin";
+            }
+
+            if (string.IsNullOrEmpty(dbName))
+            {
+                _db = client.GetDatabase(dbName);
+            }
+
+            _db = client.GetDatabase(dbName);
+            return this;
+        }
+
+        public MongoDbhandler GetDbConnection(DataSource dataSource, string userId, string pass,
+            string dbName = "admin")
+        {
+            string conString = "";
+            string port = "27017";
+            if (string.IsNullOrEmpty(dbName))
+            {
+                dbName = dataSource.defaultDatabase;
+            }
+
+            if (string.IsNullOrEmpty(dataSource.serverPort))
+            {
+                port = dataSource.serverPort;
+            }
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                conString = $"mongodb://{dataSource.serverIp}:{port}/{dbName}";
+            }
+            else
+            {
+                conString = $"mongodb://{userId}:{pass}@{dataSource.serverIp}{port}/{dbName}";
+            }
+
+            MongoClient client = new MongoClient(conString);
+            _db = client.GetDatabase(dbName);
+            return this;
+        }
+
+
+        public void Insert(string collectionName, string jsonData)
+        {
+            var collection = _db.GetCollection<BsonDocument>(collectionName);
+            var document = BsonSerializer.Deserialize<BsonDocument>(jsonData);
+            collection.InsertOne(document);
+        }
+
+        public BsonDocument Update(string collectionName, string filter, string updateFields)
+        {
+            string updateQuery = $@"{{
+                              update:  ""{collectionName}"",
+                              updates: [
+                                 {{
+                                   q: {filter},
+                                   u: {updateFields},
+                                   multi : true
+                                 }},
+                              ]
+                           }}
+                        ";
+            var commdDoc = new BsonDocumentCommand<BsonDocument>(BsonDocument.Parse(updateQuery));
+            var result = _db.RunCommand<BsonDocument>(commdDoc);
+            return result;
+        }
+
+        public void Replace(string collectionName, string filter, string updateFields)
+        {
+            var cmdBulder = new StringBuilder();
+            cmdBulder.AppendLine($"var coll = db.getCollection(\"{collectionName}\")");
+            cmdBulder.AppendLine($"coll.replaceOne({filter} , {{$set: {updateFields} }})");
+            var result = _db.RunCommand<BsonDocument>(cmdBulder.ToString());
+        }
+
+        
+
+       
+
+        public BsonDocument Delete(string collectionName, string deleteQuery)
+        {
+            string updateQuery = $@"{{
+                               delete: ""{collectionName}"",
+                               deletes: [
+                                  {{
+                                    q: {deleteQuery},
+                                    limit : 0
+                                  }}
+                               ] }}";
+            var commdDoc = new BsonDocumentCommand<BsonDocument>(BsonDocument.Parse(updateQuery));
+            var result = _db.RunCommand<BsonDocument>(commdDoc);
+            return result;
+        }
+
+        public BsonDocument Find(string collectionName, string query, string selectFields, string sortQuery)
+        {
+            string sort = "{}";
+            string projection = "{}";
+            if (!string.IsNullOrEmpty(sortQuery))
+            {
+                sort = sortQuery;
+            }
+            if (!string.IsNullOrEmpty(selectFields))
+            {
+                if (selectFields.IndexOf("{") > -1)
+                {
+                    projection = sortQuery;
+                }
+                else
+                {
+                    var arr = selectFields.Split(new string[] {"\n", ";", ","}, StringSplitOptions.RemoveEmptyEntries);
+                    var jobj = JObject.Parse("{}");
+                    foreach (var s in arr)
+                    {
+                        jobj[s] = 1;
+                    }
+                    projection = jobj.ToString();
+                }
+            }
+            string cmdText = $"{{ find: \"{collectionName}\", filter: {query},sort: {sort},projection:{projection} }}";
+            var commdDoc = new BsonDocumentCommand<BsonDocument>(BsonDocument.Parse(cmdText));
+            var result = _db.RunCommand<BsonDocument>(commdDoc);
+            return result;
+        }
+
+        public BsonDocument RunShellCommand(string commandString)
+        {
+            
+            var commdDoc = new BsonDocumentCommand<BsonDocument>(BsonDocument.Parse(commandString));
+            var result = _db.RunCommand<BsonDocument>(commdDoc);
+            return result;
+        }
+
+
+        public bool TestDbConnection()
+        {
+            bool connected = false;
+            try
+            {
+                var list = _db.ListCollections();
+                connected = true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(this, "Error connecting to Db >>", ex);
+            }
+
+            return connected;
+        }
+    }
+}
---
+++ ZenExpressoCore/TaskFlows/MongoDbTaskFlowItem.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using ZenExpresso.Helpers;
+using ZenExpressoCore.Models;
+
+namespace ZenExpressoCore.TaskFlows
+{
+    public class MongoDbTaskFlowItem : TaskFlowItem, ITaskExecutor
+    {
+        
+       
+        private string _operationType;
+        private string _mongodbScript1;
+        private string _mongodbScript2;
+        private string _mongodbScript3;
+        private string _collection;
+       
+         private MongoDbhandler dbhandler;
+
+        public MongoDbTaskFlowItem(TaskFlowItem flowItem) : base(flowItem)
+        {
+            var jsonFlowData = JObject.Parse(flowItem.flowData);
+            _operationType = jsonFlowData["operationType"].ToStringOrEmpty();
+            _mongodbScript1 = jsonFlowData["queryScript1"].ToStringOrEmpty();
+            _mongodbScript2 = jsonFlowData["queryScript2"].ToStringOrEmpty();
+            _mongodbScript3 = jsonFlowData["queryScript3"].ToStringOrEmpty();
+            var dbPass = jsonFlowData["dbPass"].ToStringOrEmpty();
+            var dbusername = jsonFlowData["dbusername"].ToStringOrEmpty();
+            var databaseName = jsonFlowData["database"].ToStringOrEmpty();
+            _collection = jsonFlowData["collection"].ToStringOrEmpty();
+            string dataSourceName = jsonFlowData["dataSource"].ToStringOrEmpty();
+            var dataSource = MemDb.Instance.GetDataSourceByName(dataSourceName);
+            if (dataSource.useRawConnectingString)
+            {
+                dbhandler = MongoDbhandler.Instance.GetDbConnection(dataSource.connectionString, databaseName);
+            }
+            else
+            {
+                dbhandler = MongoDbhandler.Instance.GetDbConnection(dataSource, dbusername, dbPass,databaseName);
+            }
+           
+        }
+
+        protected JToken ExecuteMongoQuery(string queryParam1,string queryParam2="",string queryParam3="")
+        {
+            JToken result = null;
+            switch (_operationType)
+            {
+                case "insert":
+                {
+                    dbhandler.Insert(_collection,queryParam1);
+                    result = JToken.Parse("{}");
+                    result["status"] = "00";
+                    result["message"] = "Insert Successful";
+                    break;
+                }
+                case "find":
+                {
+                    var bsonValue =  dbhandler.Find(_collection, queryParam1,queryParam2,queryParam3);
+                    var bsonJson = bsonValue.ToNormalJson();
+                    var jobj = JObject.Parse(bsonJson);
+
+                    result = jobj["cursor"]["firstBatch"];
+                    break;
+                }
+                case "update":
+                {
+                    var bsonValue =dbhandler.Update(_collection, queryParam1,queryParam2);
+                    var bsonJson = bsonValue.ToNormalJson();
+                    var jobj = JObject.Parse(bsonJson);
+                    result = jobj;
+                        break;
+                }
+                case "delete":
+                {
+                    var bsonValue= dbhandler.Delete(_collection, queryParam1);
+                    var bsonJson = bsonValue.ToNormalJson();
+                    var jobj = JObject.Parse(bsonJson);
+                    result = jobj;
+                    break;
+                }
+                case "command":
+                {
+                    var bsonValue = dbhandler.RunShellCommand(queryParam1);
+                    var bsonJson = bsonValue.ToNormalJson();
+                    var jobj = JObject.Parse(bsonJson);
+                    result = jobj;
+                    break;
+               }
+            }
+ 
+                
+            return result;
+        }
+
+        
+
+        public TaskFlowResult ExecuteResult(List<ScriptParameter> inputList, List<TaskFlowResult> resultSequence)
+        {
+            
+            var response = new TaskFlowResult();
+            response.flowItemType = flowItemType;
+            response.description = description;
+            response.controlIdentifier = controlIdentifier;
+            try
+            {
+                var queryParam1 = _mongodbScript1;
+                var queryParam2 = _mongodbScript2;
+                var queryParam3 = _mongodbScript3;
+                if (inputList.Count > 0)
+                {
+                    queryParam1 = TaskFlowUtilities.InterpolateParams(queryParam1, inputList, "\"");
+                    if (!string.IsNullOrEmpty(queryParam2))
+                    {
+                        queryParam2= TaskFlowUtilities.InterpolateParams(queryParam2, inputList,"\"");
+                    }
+                }
+                if (resultSequence.Count > 0)
+                {
+                    List<PlaceHolder> placeholders = TaskFlowUtilities.ExtractPlaceHolders(queryParam1);
+                    queryParam1 = TaskFlowUtilities.InterpolateSequenceParams(queryParam1, placeholders, resultSequence);
+                    
+                    if (!string.IsNullOrEmpty(queryParam2))
+                    {
+                        List<PlaceHolder> placeholders2 = TaskFlowUtilities.ExtractPlaceHolders(queryParam2);
+                        queryParam2= TaskFlowUtilities.InterpolateSequenceParams(queryParam2, placeholders2, resultSequence);
+                    }
+
+
+                    if (!string.IsNullOrEmpty(queryParam3))
+                    {
+                        List<PlaceHolder> placeholders3 = TaskFlowUtilities.ExtractPlaceHolders(queryParam3);
+                        queryParam3 = TaskFlowUtilities.InterpolateSequenceParams(queryParam3, placeholders3, resultSequence);
+                    }
+                }
+                var result = ExecuteMongoQuery(queryParam1,queryParam2,queryParam3);
+                response.status = "00";
+                response.message = "Success";
+                response.data = result;
+            }
+            catch (Exception ex)
+            {
+                response.status = "07";
+                Logger.Error(this, ex);
+                response.message = ex.Message +" " + ex.InnerException;
+            }
+            return response;
+        }
+    }
+}
